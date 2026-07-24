9478744d2270e56d79c4c556290e91a55f99ebf6
Chukwuemeka Bencollins Udeh <bambireborn7@gmail.com>
2026-07-24 15:42:42 +0100

    Leaderboard structure and style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,11 @@ import {Link, useLocation} from 'react-router-dom'
 import logo from '../assets/shared/Logo.svg'
 
 function Header() {
-
+  // REMOVING HEADER FROM BOTH PAGES //
   const location = useLocation()
   if (location.pathname === '/characters' || location.pathname === '/login') return null
+
+  // 
 
   return (
     <header>
@@ -27,7 +29,7 @@ function Header() {
             <a href="https://play.unity.com/en/games/6e324785-a61c-4f2d-845d-a983e36ae2ce/naija-kitchen-chaos" target="_blank" id='playNow-BTN'>
                 PLAY NOW
             </a>
-            <Link to ="/login">LOGIN</Link>
+            {/* <Link to ="/login">LOGIN</Link> */}
           </>
         ) : (
           <div style={{ width: '8%' }}></div>
