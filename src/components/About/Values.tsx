1ffa9d513c0272aa30cf67f19a4f14f165783076
Legalmoney <madukacharles2020@gmail.com>
2026-07-30 09:15:07 +0100

    Update on the Character Avatar & About us Page

--- src/components/About/Values.tsx
+++ src/components/About/Values.tsx
@@ -5,7 +5,7 @@ import community from "../../assets/about/Community.png";
 import culture from "../../assets/about/Culture.png";
 import excitement from "../../assets/about/Excitement.png";
 
-import chefImage from "../../assets/about/values-Chef.png";
+import chefImage from "../../assets/about/Values-Chef.png";
 
 export default function Values() {
   return (
